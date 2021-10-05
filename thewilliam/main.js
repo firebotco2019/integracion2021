ea097bf56eac794e46486cccabfc2527f9e291b3
Willianstars <willian.15102017@gmail.com>
2021-10-05 18:05:48 -0300

    prueba efecto matrix

---
+++ thewilliam/main.js
@@ -0,0 +1,43 @@
+const canvas = document.getElementById("CanvasMatrix");
+const ctx = canvas.getContext('2d');
+
+//set the width and height of the canvas
+const w = canvas.width = document.body.offsetWidth;
+const h = canvas.height = document.body.offsetHeight;
+
+//draw a black rectangle of width and height same as that of the canvas
+ctx.fillStyle = '#000';
+ctx.fillRect (0, 0, w, h);
+
+const cols = Math.floor(w / 20) + 1;
+const ypos = Array(cols).fill(0);
+
+function matrix () {
+    //draw a semitransparent black rectangle on top previous drawing
+    ctx.fillStyle = '#0001';
+    ctx.fillRect(0, 0, w, h,);
+
+    //set color to green and font to 15pt monospace in the drawing context
+    ctx.fillStyle = '#0f0';
+    ctx.font = '20pt monospace';
+
+    //for each column put a random character at the end
+    ypos.forEach((y, ind) => {
+
+        //generate a random character
+        const text = String.fromCharCode(Math.random() * 122);
+        
+        // x coordinate of the column, y coordinate is already given
+        const x = ind * 20;
+        //render the character at (x, y)
+        ctx.fillText(text, x, y);
+
+        //randomly resert the end of the column if it's at least 100px high
+        if (y > 100 + Math.random() * 10000) ypos[ind] = 0;
+        //otherwise just move the y coordinate for the column 20pc down
+        else ypos[ind] = y + 20;
+    });
+}
+
+//render the animation at 20 fps
+setInterval(matrix, 50);
